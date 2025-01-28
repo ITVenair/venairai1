71add961a58580d3da1e69a90cb67374b01bb1d4
ITVenair <it@venair.com>
2025-01-28 15:10:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-pro",  # Usamos el modelo correcto "gemini-pro"
+    model_name="gemini-2.0-flash-exp",  # Usamos el modelo correcto "gemini-pro"
     generation_config=generation_config,
 )
 
